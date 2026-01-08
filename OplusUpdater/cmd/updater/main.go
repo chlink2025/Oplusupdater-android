e42d5cb07318d9117cff154584ae1935b1dc0b31
chlink2025 <196435138+chlink2025@users.noreply.github.com>
2026-01-08 16:29:07 +0800

    combine

--- OplusUpdater/cmd/updater/main.go
+++ OplusUpdater/cmd/updater/main.go
@@ -33,7 +33,7 @@ var rootCmd = &cobra.Command{
 		model := getStringFlag(cmd, "model")
 		carrier := getStringFlag(cmd, "carrier")
 		region := getStringFlag(cmd, "region")
-		mode := getIntFlag(cmd, "mode")
+		mode := getStringFlag(cmd, "mode")
 		imei := getStringFlag(cmd, "imei")
 		proxy := getStringFlag(cmd, "proxy")
 
@@ -57,7 +57,7 @@ func init() {
 	rootCmd.Flags().String("region", "CN", "Server zone: CN (default), EU or IN (optional), e.g., --region=CN")
 	rootCmd.Flags().String("model", "", "Device model, e.g., --model=RMX3820")
 	rootCmd.Flags().String("carrier", "", "Found in `my_manifest/build.prop` file, under the `NV_ID` reference, e.g., --carrier=01000100")
-	rootCmd.Flags().Int("mode", 0, "Mode: 0 (stable, default) or 1 (testing), e.g., --mode=0")
+	rootCmd.Flags().String("mode", "manual", "Mode: manual (stable, default) or taste (public testing), e.g., --mode=manual")
 	rootCmd.Flags().String("imei", "", "IMEI, e.g., --imei=86429XXXXXXXX98")
 	rootCmd.Flags().StringP("proxy", "p", "", "Proxy server, e.g., --proxy=type://@host:port or --proxy=type://user:password@host:port, support http and socks proxy")
 }
